20620725f6586c7f67cfdfe210c29d47dbbb9c9b
nick@npkessler.com <nick@npkessler.com>
2018-04-23 12:56:36 -0700

    Added model including all modified images joined into a single DF, with grid search and cross validation to optimize results, using both Random Forest and Extra Trees classifiers.
    Saved fitted models to pickle files. Added ./pickled_data folder for pickled files.
    
    Updated ds_functions.py, added ds_settings.py for storing settings.

--- ds_functions.py
+++ ds_functions.py
@@ -9,10 +9,14 @@ import matplotlib.pyplot as plt
 import itertools
 import re
 import seaborn as sns
+import pickle
+import gzip
+import bz2
 
-from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier, AdaBoostClassifier
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier, AdaBoostClassifier, ExtraTreesClassifier
+from sklearn.linear_model import LogisticRegressionCV
 from sklearn.neural_network import MLPClassifier
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import *
 from sklearn.metrics import classification_report, accuracy_score, confusion_matrix
 
 def zip_to_df(filepath, sep=',', encoding='auto', header='infer'):
@@ -55,34 +59,6 @@ def cm_plot(cm, classes=None, title=None, cmap=plt.cm.Reds, figsize=(10,8), norm
     p = plt.xlabel('Predicted label')
     return p
 
-def cm_to_df(cm, classes=None):
-    """Converts a sklearn confusion matrix into a pandas DF"""
-    df = pandas.DataFrame(cm, index=classes, columns=classes)
-    return df
-    
-def create_test_train(df):
-    """Creates a train-test split from a dataframe. First one-hot encodes, then splits. Returns 
-    X_trian, X-test, y_train, y_test"""
-    # create X and y
-    y = df.country_group
-    X = df.drop(['id', 'country_group', 'country_destination', 'date_account_created', 'timestamp_first_active', 
-            'date_first_booking'], axis=1)
-    
-    # specify type for signup flow
-    X['signup_flow'] = X['signup_flow'].astype('category')
-    # One hot encoding of categorical variables 
-    # using Pandas get_dummies method
-    X_onehot = pd.get_dummies(X) 
-    X_onehot.age = X_onehot.age.fillna(0)
-    X_onehot.days_to_book = X_onehot.days_to_book.fillna(100000)
-    
-    # create test train split
-    from sklearn.model_selection import train_test_split
-    X_train, X_test, y_train, y_test = train_test_split(
-        X_onehot, y, test_size=0.1, random_state=1)
-    # return test and train groups
-    return X_train, X_test, y_train, y_test
-
 
 def images_to_df(filepath, img_size=(200, 200), pics_per_class=None):
     """Extracts images from data file and converts each into a pandas dataframe. 
@@ -96,6 +72,7 @@ def images_to_df(filepath, img_size=(200, 200), pics_per_class=None):
     # Exception: Path ./C:/Users/Nick/kaggle/plant-seedlings/colorized not found
     # extrac the images through each folder. 
     out_folder = './' + re.search('^.*/(.*?)\..*$', filepath).group(1)
+    print('Out Folder:', out_folder)
     
     if not os.path.isdir(out_folder):
         try:
@@ -127,6 +104,44 @@ def images_to_df(filepath, img_size=(200, 200), pics_per_class=None):
     ret_df.columns = cols
     return ret_df.reset_index(drop=True)
 
+def get_data(filepath, read_pickle=False, write_pickle=False):
+    if read_pickle:
+        big_data = pd.read_pickle(filepath)
+    else:
+        data = {}
+        for file in filepath: #, alpha_file, colorized_file]
+            name = re.search('^.*/(.*?)\.zip$', file).group(1)
+            data[name]= images_to_df(file, 
+                                    img_size=img_size, 
+                                     pics_per_class=pics_per_class)
+        if len(data) > 1:
+            for i in range(0, len(data) - 1):
+                big_data = data[i].merge(data[i+1], on=['id', 'class'])
+        else:
+            big_data = data[0]
+    
+        if write_pickle:
+            big_data.to_pickle(pickle_file)
+    return big_data
+
+
+def save_pickle(obj, out_file, zip_class=None):
+    if zip_class != None:
+        f = zip_class.open(out_file, 'wb')
+    else:
+        f = open(out_file, 'wb')
+    pickle.dump(obj, f)
+    f.close()
+        
+def load_pickle(in_file, zip_class=None):
+    if zip_class != None:
+        f = zip_class.open(in_file, 'rb')
+    else:
+        f = open(in_file, 'rb')
+    ret = pickle.load(f)
+    f.close()
+    return ret
+
 
 # Create a class that will take in the data and return a trained model with 
 # results
@@ -134,53 +149,66 @@ class sk_cls():
     """sk_cls takes an sk-learn classifier, pandas dataset, and list of features and
     target variables, and builds a trained model with results statistics"""
     
-    def __init__(self, cls, data, X, y, ratio=0.1):
+    def __init__(self, cls, data, X, y, ratio=0.1, cv=None):
         # check data
-        if data.isnull().sum().any():
-            raise Exception('Data contains NA values. Correct before attempting to model')
+#         if data.isnull().sum().any():
+#             raise Exception('Data contains NA values. Correct before attempting to model')
         
         # set initial variables
         self.model = cls
         self.score = 0
         self.conf_matrix = None
         self.cls_report = None
+        self.y_pred = None
         self.__tgt = y
-        self.__ftr = X
-        self.__sorted_classes = sorted(y.unique())
+        self.__ftr = list(X)
+        self.__sorted_classes = sorted(data[self.__tgt].unique())
+        self.__cv = cv
         
         # create train test split
         self.__X_train, self.__X_test, self.__y_train, self.__y_test = train_test_split(
-            data.loc[:, self.__ftr],
-            data.loc[:, self.__tgt], test_size=ratio)
+            data[self.__ftr],
+            data[self.__tgt], test_size=ratio)
     
     def train_cls(self, cls, X_train, X_test, y_train, y_test):
         """Fir the model and """
-        cls.fit(X_train, y_train)
-        y_pred = cls.predict(X_test)
-        sc = accuracy_score(y_test, y_pred)
-        cm = confusion_matrix(y_test, y_pred)
-        cr = classification_report(y_test, y_pred)
-        
+        if self.__cv == None:
+            # until I can figure out why I'm getting NAs I'll use fillna
+            cls.fit(X_train.fillna(0), y_train)
+            y_pred = cls.predict(X_test)
+            sc = accuracy_score(y_test, y_pred)
+            cm = confusion_matrix(y_test, y_pred)
+            cr = classification_report(y_test, y_pred)
+        else:
+            X = pd.concat([X_train, X_test])
+            y = pd.concat([y_train, y_test])
+            y_pred = cross_val_predict(cls, X.fillna(0), y, cv=self.__cv, n_jobs=2)
+            sc = accuracy_score(y, y_pred)
+            cr = classification_report(y_train, y_pred)
+            cm = confusion_matrix(y_train, y_pred)
+            
         results = {
             'cls':cls,
             'sc':sc,
             'cm':cm,
-            'cr':cr
+            'cr':cr,
+            'y_pred':y_pred
         }
         return results
     
     def run(self):
         """Run the model"""
-        results = self.train_cls(self.model, 
+        results = self.train_cls(self.model,
                                  self.__X_train, self.__X_test, 
                                  self.__y_train, self.__y_test)
         self.model = results['cls']
         self.score = results['sc']
         self.conf_matrix = results['cm']
         self.cls_report = results['cr']
+        self.y_pred = results['y_pred']
         
     def show(self, plot=True, title=None, cbar=False, normed=True):
-        """outputs all results including a cm plot"""
+        """outputs all results including a cm plot if enabled"""
         if self.score == 0:
             raise Exception('Model has not been run. Use sk_cls.run() first')
             
@@ -198,3 +226,4 @@ class sk_cls():
             plt.show()
         else:
             print(self.conf_matrix)
+            
---
+++ ds_settings.py
@@ -0,0 +1,19 @@
+# settings 
+
+# img_size is dimension to resize images to.
+img_size = (175,175)
+
+# number of pics in each class to use. use None for all images per class
+#pics_per_class = 300
+pics_per_class = None
+
+# set zipped datafile location
+train_file = 'E:/git-repos/plant-seedlings/train.zip'
+maxrgb_file = 'E:/git-repos/plant-seedlings/maxrgb.zip'
+alpha_file = 'E:/git-repos//plant-seedlings/alpha.zip'
+green_yellow_file = 'E:/git-repos/plant-seedlings/green_yellow.zip'
+colorized_file = 'E:/git-repos/plant-seedlings/colorized.zip'
+
+# pickle file locations
+use_pickle = True
+pickle_file='E:/git-repos/plant-seedlings/pickled_data/big_data.pkl'
